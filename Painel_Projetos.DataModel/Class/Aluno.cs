9d9e4ab6e7784815bb927385e5d471b81a829eeb
ericknovais <erick.hora@Outlook.com>
2020-03-10 06:59:23 -0300

    Criado class: Aluno Criado Interface: IAlunoRepository

---
+++ Painel_Projetos.DataModel/Class/Aluno.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Painel_Projetos.DomainModel.Class
+{
+    [Table("Alunos")]
+    public class Aluno : EntityBase
+    {
+        public int RA { get; set; }
+        public string Nome { get; set; }
+        public Curso Curso { get; set; }
+        public int CursoID { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Email { get; set; }
+
+        public override void Validar()
+        {
+            base.Validar();
+        }
+    }
+}
