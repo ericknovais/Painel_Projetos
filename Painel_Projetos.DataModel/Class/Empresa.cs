0e2b59ffca7c931d0ebcb23496fe008f734abbff
ericknovais <erick.hora@Outlook.com>
2020-03-11 20:24:31 -0300

    Criado classes:   Empresa   Representante Criado Repository:   IEmpresaRepository   IRepresentanteRepository

---
+++ Painel_Projetos.DataModel/Class/Empresa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Painel_Projetos.DomainModel.Class
+{
+    [Table("Empresas")]
+    public class Empresa : EntityBase
+    {
+        public string CNPJ { get; set; }
+        public string RazaoSocial { get; set; }
+        public Representante Representante { get; set; }
+        public int RepresentanteId { get; set; }
+    }
+}
---
+++ Painel_Projetos.DataModel/Class/Representante.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Painel_Projetos.DomainModel.Class
+{
+    public class Representante : EntityBase
+    {
+        public string Nome { get; set; }
+        public string E mail { get; set; }
+
+    }
+}
